5c32bc46ca6d442d852b4b0813316e95ee583fc4
Esteban Lizaso <estebanlizaso@gmail.com>
2022-05-29 02:11:36 -0300

    Agregué algunas verificaciones. Falta la del horario del local

--- js/formulario.js
+++ js/formulario.js
@@ -1,4 +1,4 @@
-document.getElementById("reserva").addEventListener('submit', validarReserva);
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("reserva").addEventListener('submit', validarReserva);})
 
 function validarReserva(evento){
     evento.preventDefault();
@@ -10,15 +10,52 @@ function validarReserva(evento){
     }
 
     var email = document.getElementById("email").value;
+    if (email.length == 0) {
+        alert('Por favor, ingrese un email de contacto.')
+        document.reserva.email.focus();
+        return;
+    }
 
-
+    var telefono = document.getElementById("telefono").value;
+    if (telefono.length == 0) {
+        alert('Por favor, ingrese un teléfono de contacto.')
+        document.reserva.telefono.focus();
+        return;
+    } else if (isNaN(parseInt(telefono))){
+        alert('Por favor, ingrese un teléfono válido.')
+        document.reserva.telefono.focus();
+        return;
+    }
+    
     var personas = document.getElementById("personas").value;
-    if (0 < personas) {
+    if (personas < 1) {
         alert('La cantidad de personas asitentes debe ser mayor a 1')
         document.reserva.personas.focus();
-        return
-    } else if (personas > 12) {
+        return;
+    } else if (personas > 50) {
         alert('La cantidad de personas que ha marcado supera la capacidad de nuestro local. Por favor pongáse en contacto con nosotros por teléfono o mail para que podamos ofrecerle alguna otra opción acorde a sus necesidades.')
-        return
+        return;
     }
+
+    var fecha = document.getElementById("fecha").value;
+    var dia = new Date(fecha).getDay();
+    if (fecha.length == 0) {
+        alert('Debe seleccionar un día para la reserva.')
+        document.reserva.fecha.focus();
+        return;
+    } else if (dia == 6) {
+        alert('Los domingos La Taberna de Marduk se encuentra cerrada. Debe seleccionar otro día.')
+        document.reserva.fecha.focus();
+        return;
+    }
+
+    var hora = document.getElementById("hora").value;
+    //if 
+
+
+
+
+
+    alert("Muchas gracias por su reserva.")
+    this.submit();
 }
